c04f55ec9b02ba5d34a45b1d6dc9f6353d2771a7
Alexandr Fomin <146896388+AlexF0min@users.noreply.github.com>
2025-06-17 09:06:49 +0200

    Initial commit

---
+++ ConditionStatements/ConditionStatements1/Program.cs
@@ -0,0 +1,27 @@
+﻿Random dice = new Random();
+
+int diceRoll1 = dice.Next(1, 7);
+int diceRoll2 = dice.Next(1, 7);
+int diceRoll3 = dice.Next(1, 7);
+
+int totalRolls = diceRoll1 + diceRoll2 + diceRoll3;
+
+System.Console.WriteLine($"Dice rolls: {diceRoll1} + {diceRoll2} + {diceRoll3} = {totalRolls}");
+
+if ((diceRoll1 == diceRoll2) || (diceRoll2 == diceRoll3) || (diceRoll1 == diceRoll3))
+{
+    System.Console.WriteLine("You roled doubles. + 2 bonus to total!");
+}
+
+if (totalRolls > 14)
+{
+    System.Console.WriteLine("You win!");
+}
+if (totalRolls < 15)
+{
+    System.Console.WriteLine("You loose!");
+}
+
+
+
+
---
+++ ExirciseMethods/Methods/Program.cs
@@ -0,0 +1,13 @@
+﻿// I needed to find a suitable method to find a larger number.
+
+int firstValue = 500;
+int secondValue = 600;
+
+int largerValueDefined = System.Math.Max(firstValue, secondValue); 
+
+int largerValuePrint = largerValueDefined;
+
+System.Console.WriteLine(largerValuePrint);
+
+
+
